262b9fca1387a627a7df04d621dcc739994a9d9d
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2025-09-16 15:12:05 +0300

    fix(remote): run test from terminal

--- remote/src/test/java/com/onlyoffice/docs/jira/remote/web/controller/AbstractControllerTest.java
+++ remote/src/test/java/com/onlyoffice/docs/jira/remote/web/controller/AbstractControllerTest.java
@@ -21,12 +21,11 @@ package com.onlyoffice.docs.jira.remote.web.controller;
 import com.onlyoffice.docs.jira.remote.client.jira.JiraClient;
 import com.onlyoffice.docs.jira.remote.security.XForgeTokenRepository;
 import com.redis.testcontainers.RedisContainer;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -40,7 +39,7 @@ import org.testcontainers.utility.DockerImageName;
 
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
-@ExtendWith(MockitoExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @SpringBootTest
 @Testcontainers
 public abstract class AbstractControllerTest {
